69043d4946bdc935a0a3d50a95da6daa34535dc0
ackermanaakriti <bhandariaakriti897@gmail.com>
2022-04-23 22:31:21 +0545

    first

--- src/App.js
+++ src/App.js
@@ -2,22 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
+  const arr= new Array(9).fill('hello');
+  console.log(arr);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <p>hello world</p>
     </div>
   );
 }
